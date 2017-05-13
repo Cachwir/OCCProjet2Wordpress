48d7718e13c841332dea24999b0b418233b6f17c
Cachwir <cachwir@gmx.com>
2017-05-13 23:47:39 +0200

    Complete project moved on github

---
+++ wp-content/themes/rttheme15/404.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+* rt-theme 404 
+*/
+get_header();  
+?>
+
+<?php get_template_part( 'sub_page_header', 'sub_page_header_file' );?> 
+	
+ 
+			<h1 style="font-size:120px;">404</h1>
+			<h6><?php _e( 'Toutes nos excuses. La page que vous souhaitiez atteindre n\'existe pas.', 'rt_theme'); ?></h6><br /><br /><br />
+    
+<?php get_footer();?>
